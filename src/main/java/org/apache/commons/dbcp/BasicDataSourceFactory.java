dd1f933bae02e7bf82d9b35f9a4f1f5069737685
wenshao <szujobs@hotmail.com>
2012-10-23 08:40:55 +0800

    improve DBCP compatible

--- src/main/java/org/apache/commons/dbcp/BasicDataSourceFactory.java
+++ src/main/java/org/apache/commons/dbcp/BasicDataSourceFactory.java
@@ -2,11 +2,18 @@ package org.apache.commons.dbcp;
 
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
 import com.alibaba.druid.pool.DruidDataSourceFactory;
 
 public class BasicDataSourceFactory extends DruidDataSourceFactory {
-
-    public static BasicDataSource createDataSource(Properties properties) throws Exception {
+    public static DataSource createDataSource(Properties properties) throws Exception {
+        BasicDataSource dataSource = new BasicDataSource();
+        config(dataSource, properties);
+        return dataSource;
+    }
+    
+    protected DataSource createDataSourceInternal(Properties properties) throws Exception {
         BasicDataSource dataSource = new BasicDataSource();
         config(dataSource, properties);
         return dataSource;
